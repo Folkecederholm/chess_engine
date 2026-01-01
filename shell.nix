032fa2ae478f284812fc77625196ed4190d758d9
Folke Cederholm <folkecederholm@gmail.com>
2026-01-01 18:03:53 +0100

    Modify shell to add clang if Linux environmet. Haven't tested on MacOS yet.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,13 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  system = builtins.currentSystem;
+  isMac = builtins.substring 6 6 system == "darwin";
+  clangPackage = if isMac then [] else [pkgs.clang];
+in
+
 pkgs.mkShell {
   packages = [
     pkgs.rustc
-  ];
+  ] ++ clangPackage;
 }
